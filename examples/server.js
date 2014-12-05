baaf6aef4f78b3beb77d0805d32d6114cd4bae47
neutrino <jrusev@gmail.com>
2014-12-05 14:52:20 +0200

    initial commit

---
+++ examples/server.js
@@ -0,0 +1,63 @@
+var Duplex = require('stream').Duplex;
+var connect = require('connect');
+var argv = require('optimist').argv;
+var livedb = require('livedb');
+var livedbMongo = require('livedb-mongo');
+var http = require('http');
+var morgan = require('morgan');
+var serveStatic = require('serve-static');
+var sharejs = require('../lib');
+
+var app = connect();
+app.use(morgan());
+app.use('/srv', serveStatic(sharejs.scriptsDir));
+app.use(serveStatic("" + __dirname + "/public"));
+
+var backend = livedb.client(livedb.memory());
+// backend = livedb.client livedbMongo('localhost:27017/test?auto_reconnect', safe:false)
+
+var share = sharejs.server.createClient({
+    backend: backend
+});
+
+var server = http.createServer(app);
+var WebSocketServer = require('ws').Server;
+var wss = new WebSocketServer({
+    server: server
+});
+
+wss.on('connection', function (client) {
+    var stream;
+    stream = new Duplex({
+        objectMode: true
+    });
+    stream._write = function (chunk, encoding, callback) {
+        console.log('s->c ', chunk);
+        client.send(JSON.stringify(chunk));
+        return callback();
+    };
+    stream._read = function () {};
+    stream.headers = client.upgradeReq.headers;
+    stream.remoteAddress = client.upgradeReq.connection.remoteAddress;
+    client.on('message', function (data) {
+        console.log('c->s ', data);
+        return stream.push(JSON.parse(data));
+    });
+    stream.on('error', function (msg) {
+        return client.close(msg);
+    });
+    client.on('close', function (reason) {
+        stream.push(null);
+        stream.emit('close');
+        console.log('client went away');
+        return client.close(reason);
+    });
+    stream.on('end', function () {
+        return client.close();
+    });
+    return share.listen(stream);
+});
+
+var port = argv.p || 7007;
+server.listen(port);
+console.log("Listening on http://localhost:" + port + "/");
